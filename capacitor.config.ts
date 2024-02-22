bedb95aee10d8b03b6d765e25bfb1160e643ac97
melih kocaturk <melih.kocaturk@anextour.com>
2024-02-22 16:54:26 +0300

    refactor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'bad-horsie-mobile',
+  appName: 'AtClub',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
